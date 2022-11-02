638ee3611cb58c8a61832c38936140aff200aa2d
David Rokeby <65196303+david-rokeby@users.noreply.github.com>
2022-11-02 11:47:01 -0400

    Update math_nodes.py
    refactored

--- dpg_system/math_nodes.py
+++ dpg_system/math_nodes.py
@@ -48,39 +48,35 @@ class ArithmeticNode(Node):
     def __init__(self, label: str, data, args):
         super().__init__(label, data, args)
 
-        self.operand = 0
-
-        self.input = self.add_input('in', trigger_node=self)
-        self.operand_input = self.add_input('', widget_type='drag_float')
-        self.operand_input.add_callback(self.change_operand)
+        self.operand = self.arg_as_number(default_value=0.0)
+        self.input = self.add_input('in', triggers_execution=True)
+        self.operand_input = self.add_input('', widget_type='drag_float', default_value=self.operand, callback=self.operand_changed)
         self.output = self.add_output("")
+        self.operations = {'+': self.add, '-': self.subtract, '!-': self.inverse_subtract,
+                           '*': self.multiply, '/': self.divide, '//': self.int_divide,
+                           '!/': self.inverse_divide, 'pow': self.power, '^': self.power,
+                           'min': self.min, 'max': self.max, 'mod': self.mod, '%': self.mod}
+        if label in self.operations:
+            self.operation = self.operations[label]
+        else:
+            self.operation = self.operations['+']
 
-        if label == '+':
-            self.operation = self.add
-        elif label == '-':
-            self.operation = self.subtract
-        elif label == '!-':
-            self.operation = self.inverse_subtract
-        elif label == '*':
-            self.operation = self.multiply
-        elif label == '/':
-            self.operation = self.divide
-        elif label == '//':
-            self.operation = self.int_divide
-        elif label == '!/':
-            self.operation = self.inverse_divide
-        elif label == 'pow':
-            self.operation = self.power
-        elif label == '^':
-            self.operation = self.power
-        elif label == 'min':
-            self.operation = self.min
-        elif label == 'max':
-            self.operation = self.max
-        elif label == 'mod':
-            self.operation = self.mod
-        elif label == '%':
-            self.operation = self.mod
+    def operand_changed(self):
+        self.operand = self.operand_input.get_widget_value()
+
+    def execute(self):
+        if self.operand_input.fresh_input:
+            self.operand = self.operand_input.get_received_data()
+            if type(self.operand) == list:
+                self.operand = list_to_array(self.operand)
+
+        input_value = self.input.get_received_data()
+        t = type(input_value)
+        if t == list:
+            input_value = list_to_array(input_value)
+
+        output_value = self.operation(input_value, self.operand)
+        self.output.send(output_value)
 
     def mod(self, a, b):
         if b == 0:
@@ -127,30 +123,6 @@ class ArithmeticNode(Node):
     def power(self, a, b):
         return math.pow(a, b)
 
-    def custom(self):
-        in_value, t = decode_arg(self.args, 0)
-        if t in [int, float]:
-            self.operand_input.widget.set(in_value)
-
-    def change_operand(self):
-        self.operand = self.operand_input.get_widget_value()
-
-    def execute(self):
-        if self.operand_input.fresh_input:
-            if type(self.operand_input.data) == np.ndarray:
-                self.operand = self.operand_input.get_received_data()
-            else:
-                self.operand = self.operand_input.get_widget_value()
-
-        input_value = self.input.get_received_data()
-        t = type(input_value)
-        if t == list:
-            input_value = list_to_array(input_value)
-
-        output_value = self.operation(input_value, self.operand)
-        self.output.set(output_value)
-        self.send_outputs()
-
 
 class ComparisonNode(Node):
     @staticmethod
@@ -161,25 +133,33 @@ class ComparisonNode(Node):
     def __init__(self, label: str, data, args):
         super().__init__(label, data, args)
 
-        self.operand = 0
-
-        self.input = self.add_input('in', trigger_node=self)
-        self.operand_input = self.add_input('', widget_type='drag_float')
-        self.operand_input.add_callback(self.change_operand)
+        self.operand = self.arg_as_number(default_value=0.0)
+        self.input = self.add_input('in', triggers_execution=True)
+        self.operand_input = self.add_input('', widget_type='drag_float', default_value=self.operand, callback=self.operand_changed)
         self.output = self.add_output("")
+        self.operations = {'>': self.greater, '>=': self.greater_equal, '==': self.equal,
+                           '<': self.less, '<=': self.less_equal, '!=': self.not_equal}
+        if label in self.operations:
+            self.operation = self.operations[label]
+        else:
+            self.operation = self.operations['>']
 
-        if label == '>':
-            self.operation = self.greater
-        elif label == '>=':
-            self.operation = self.greater_equal
-        elif label == '==':
-            self.operation = self.equal
-        elif label == '<':
-            self.operation = self.less
-        elif label == '<=':
-            self.operation = self.less_equal
-        elif label == '!=':
-            self.operation = self.not_equal
+    def operand_changed(self, s, a, u):
+        self.operand = self.operand_input.get_widget_value()
+
+    def execute(self):
+        if self.operand_input.fresh_input:
+            self.operand = self.operand_input.get_received_data()
+            if type(self.operand) == list:
+                self.operand = list_to_array(self.operand)
+
+        input_value = self.input.get_data()
+        t = type(input_value)
+        if t == list:
+            input_value = list_to_array(input_value)
+
+        output_value = self.operation(input_value, self.operand)
+        self.output.send(output_value)
 
     def greater(self, a, b):
         return a > b
@@ -199,31 +179,6 @@ class ComparisonNode(Node):
     def not_equal(self, a, b):
         return a != b
 
-    def change_operand(self):
-        self.operand = self.operand_input.get_widget_value()
-
-    def custom(self):
-        in_value, t = decode_arg(self.args, 0)
-        if t in [int, float]:
-            self.operand_input.widget.set(in_value)
-            self.operand = in_value
-
-    def execute(self):
-        if self.operand_input.fresh_input:
-            self.operand = self.operand_input.get_received_data()
-        input_value = self.input.get_received_data()
-
-        t = type(input_value)
-        if t == np.ndarray:
-            output_value = self.np_operation(input_value, self.operand)
-        elif t == list:
-            a = list_to_array(input_value)
-            output_value = self.np_operation(a, self.operand)
-        else:
-            output_value = self.operation(input_value, self.operand)
-
-        self.output.send(output_value)
-
 
 class OpSingleNode(Node):
     @staticmethod
@@ -234,108 +189,82 @@ class OpSingleNode(Node):
     def __init__(self, label: str, data, args):
         super().__init__(label, data, args)
 
-        self.input = self.add_input('in', trigger_node=self)
+        self.input = self.add_input('in', triggers_execution=True)
         self.output = self.add_output("")
 
-        if label == 'log10':
-            self.operation = self.log10
-            self.np_operation = self.np_log10
-        elif label == 'log2':
-            self.operation = self.log2
-            self.np_operation = self.np_log2
-        elif label == 'exp':
-            self.operation = self.exp
-            self.np_operation = self.np_exp
-        elif label == 'inverse':
-            self.operation = self.inverse
-            self.np_operation = self.np_inverse
-        elif label == 'abs':
-            self.operation = self.abs
-            self.np_operation = self.np_abs
-        elif label == 'sqrt':
-            self.operation = self.square_root
-            self.np_operation = self.np_square_root
-        elif label == 'norm':
-            self.operation = self.normalize
-            self.np_operation = self.np_normalize
-
-    def np_normalize(self, a):
-        with np.errstate(divide='ignore'):
-            result = np.divide(a, np.linalg.norm(a))
-        return result
-
-    def normalize(self, a):
-        return 1.0
-
-    def np_log10(self, a):
-        with np.errstate(divide='ignore'):
-            result = np.log10(np.abs(a))
-        return result
-
-    def log10(self, a):
-        if a > 0.0:
-            return math.log10(a)
-        return -math.inf
-
-    def np_log2(self, a):
-        with np.errstate(divide='ignore'):
-            result = np.log2(np.abs(a))
-        return result
-
-    def log2(self, a):
-        if a > 0.0:
-            return math.log2(a)
-        return -math.inf
-
-    def np_inverse(self, a):
-        with np.errstate(divide='ignore'):
-            result = np.divide(1, a)
-        return result
-
-    def inverse(self, a):
-        if a == 0:
-            return math.inf
-        return 1 / a
-
-    def np_exp(self, a):
-        return np.exp(a)
-
-    def exp(self, a):
-        return math.exp(a)
-
-    def np_abs(self, a):
-        return np.abs(a)
-
-    def abs(self, a):
-        if a >= 0:
-            return a
-        return -a
-
-    def np_square_root(self, a):
-        signs = np.sign(a)
-        result = np.sqrt(np.abs(a))
-        return result * signs
-
-    def square_root(self, a):
-        if a > 0:
-            return math.sqrt(a)
-        return - math.sqrt(-a)
-
-    def custom(self):
-        pass
+        self.operations = {'log10': self.log10, 'log2': self.log2, 'exp': self.exp,
+                           'inverse': self.inverse, 'abs': self.abs,
+                           'sqrt': self.square_root, 'norm': self.normalize}
+        if label in self.operations:
+            self.operation = self.operations[label]
+        else:
+            self.operation = self.operations['log10']
 
     def execute(self):
         # get values from static attributes
         input_value = self.input.get_received_data()
         t = type(input_value)
-        if t == np.ndarray:
-            output_value = self.np_operation(input_value)
-        elif t == list:
-            a = list_to_array(input_value)
-            output_value = self.np_operation(a)
-        else:
-            output_value = self.operation(input_value)
+        if t == list:
+            input_value = list_to_array(input_value)
+        elif t in [int, bool, np.int64, np.bool_]:
+            input_value = float(input_value)
+        output_value = self.operation(input_value)
         self.output.send(output_value)
+
+    def normalize(self, a):
+        if type(a) == np.ndarray:
+            with np.errstate(divide='ignore'):
+                result = np.divide(a, np.linalg.norm(a))
+            return result
+        return 1.0
+
+    def log10(self, a):
+        if type(a) == np.ndarray:
+            with np.errstate(divide='ignore'):
+                result = np.log10(np.abs(a))
+            return result
+        if a > 0.0:
+            return math.log10(a)
+        return -math.inf
+
+    def log2(self, a):
+        if type(a) == np.ndarray:
+            with np.errstate(divide='ignore'):
+                result = np.log2(np.abs(a))
+            return result
+        if a > 0.0:
+            return math.log2(a)
+        return -math.inf
+
+    def inverse(self, a):
+        if type(a) == np.ndarray:
+            with np.errstate(divide='ignore'):
+                result = np.divide(1, a)
+            return result
+        if a == 0:
+            return math.inf
+        return 1 / a
+
+    def exp(self, a):
+        if type(a) == np.ndarray:
+            return np.exp(a)
+        return math.exp(a)
+
+    def abs(self, a):
+        if type(a) == np.ndarray:
+            return np.abs(a)
+        if a >= 0:
+            return a
+        return -a
+
+    def square_root(self, a):
+        if type(a) == np.ndarray:
+            signs = np.sign(a)
+            result = np.sqrt(np.abs(a))
+            return result * signs
+        if a > 0:
+            return math.sqrt(a)
+        return - math.sqrt(-a)
 
 
 class OpSingleTrigNode(Node):
@@ -348,25 +277,30 @@ class OpSingleTrigNode(Node):
         super().__init__(label, data, args)
 
         self.use_degrees = True
+        self.degrees_to_radians = math.pi / 180
 
-        self.input = self.add_input('in', trigger_node=self)
+        self.input = self.add_input('in', triggers_execution=True)
         self.use_degrees_property = self.add_property('degrees', widget_type='checkbox', default_value=self.use_degrees)
         self.output = self.add_output("out")
+        self.operations = {'sin': self.sin, 'cos': self.cos, 'asin': self.asin,
+                           'acos': self.acos, 'tan': self.tan, 'atan': self.atan}
+        if label in self.operations:
+            self.operation = self.operations[label]
+        else:
+            self.operation = self.operations['sin']
 
-        if label == 'sin':
-            self.operation = self.sin
-        elif label == 'cos':
-            self.operation = self.cos
-        elif label == 'asin':
-            self.operation = self.asin
-        elif label == 'acos':
-            self.operation = self.acos
-        elif label == 'tan':
-            self.operation = self.tan
-        elif label == 'atan':
-            self.operation = self.atan
+    def execute(self):
+        # get values from static attributes
+        self.use_degrees = self.use_degrees_property.get_widget_value()
 
-        self.degrees_to_radians = math.pi / 180
+        input_value = self.input.get_received_data()
+        t = type(input_value)
+        if t == list:
+            input_value = list_to_array(input_value)
+        elif t in [int, bool, np.int64, np.bool_]:
+            input_value = float(input_value)
+        output_value = self.operation(input_value)
+        self.output.send(output_value)
 
     def sin(self, a):
         t = type(a)
@@ -455,19 +389,4 @@ class OpSingleTrigNode(Node):
                 return math.atan(a) / self.degrees_to_radians
             else:
                 return math.atan(a)
-
-    def custom(self):
-        pass
-
-    def execute(self):
-        # get values from static attributes
-        self.use_degrees = self.use_degrees_property.get_widget_value()
-        input_value = self.input.get_received_data()
-        t = type(input_value)
-        if t == list:
-            input_value = list_to_array(input_value)
-        elif t in [int, bool, np.int64, np.bool_]:
-            input_value = float(input_value)
-        output_value = self.operation(input_value)
-        self.output.send(output_value)
 
